b09b452b0370f7e257d5776b0232c3214b767877
zentia <363476658@qq.com>
2019-06-17 19:20:08 +0800

    earth

--- Assets/Scripts/UILoadVideo.cs
+++ Assets/Scripts/UILoadVideo.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Video;
 using System.IO;
+using System.Collections;
 
 public class UILoadVideo : MonoBehaviour
 {
     public string[] m_IdArray;
-    public int m_LastReduceFrameCount;
     private int m_Index = 0;
     public static UILoadVideo Instance;
+    public GameObject m_Quad;
     public bool IsFinished
     {
         get
@@ -17,34 +17,19 @@ public class UILoadVideo : MonoBehaviour
         }
     }
     public VideoPlayer m_videoPlayer;
-    private void Awake()
+    private void Start()
     {
         Instance = this;
-        int width = Screen.width;
-        int height = Screen.height;
-        int designWidth = 960;
-        int designHeight = 640;
-        float s1 = (float)designWidth / (float)designHeight;
-        float s2 = (float)width / (float)height;
-        if (s1 < s2)
-        {
-            designWidth = Mathf.FloorToInt(designHeight * s2);
-        }
-        else if (s1 > s2)
-        {
-            designHeight = Mathf.FloorToInt(designWidth / s2);
-        }
-        float contentScale = (float)designWidth / width;
-        RectTransform rectTransform = transform as RectTransform;
-        if (rectTransform != null)
-        {
-            rectTransform.sizeDelta = new Vector2(designWidth, designHeight);
-        }
+        float scale = ((float)Screen.width / Screen.height) / (1920f / 1080f);
+        if (scale > 0)
+            Camera.main.orthographicSize /= scale;
         m_videoPlayer = GetComponent<VideoPlayer>();
-
-        //m_videoPlayer.loopPointReached += OnLoopPointReached;
-        m_videoPlayer.sendFrameReadyEvents = true;
         m_videoPlayer.prepareCompleted += OnPrepareCompleted;
+        StartCoroutine(DelayPlay());
+    }
+    private IEnumerator DelayPlay()
+    {
+        yield return new WaitForSeconds(3);
         PlayVideo(m_videoPlayer);
     }
     private void PlayVideo(VideoPlayer source)
@@ -59,10 +44,9 @@ public class UILoadVideo : MonoBehaviour
         {
             source.url = string.Format("{0}/{1}", Application.streamingAssetsPath, uiVideoPathTables);
         }
-        m_frame = 0;
         source.Prepare();
     }
-    public void OnLoopPointReached(VideoPlayer source)
+    public void OnStop(VideoPlayer source)
     {
         if (m_Index == 1)
         {
@@ -73,27 +57,26 @@ public class UILoadVideo : MonoBehaviour
         {
             source.Stop();
             m_Index = 3;
+
         }
     }
-    private ulong m_frame = 0;
     private void Update()
     {
         if (m_videoPlayer == null || !m_videoPlayer.isPrepared)
             return;
-        if (m_frame == m_videoPlayer.frameCount)
+        if (!m_videoPlayer.isPlaying)
         {
-            OnLoopPointReached(m_videoPlayer);
-            m_frame++;
+            OnStop(m_videoPlayer);
             return;
         }
-        if (m_frame > m_videoPlayer.frameCount)
-            return;
-        m_frame++;
+        if (Input.touchCount > 0)
+        {
+            OnStop(m_videoPlayer);
+        }
     }
     private void OnPrepareCompleted(VideoPlayer source)
     {
-        var rawImage = GetComponent<RawImage>();
-        rawImage.texture = source.texture;
+        m_Quad.SetActive(false);
         source.Play();
     }
 }
--- Assets/Shaders/UITransparent.shader
+++ Assets/Shaders/UITransparent.shader
@@ -14,8 +14,6 @@
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
 
             #include "UnityCG.cginc"
 
@@ -40,16 +38,12 @@
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
                 fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
                 return col;
             }
             ENDCG
